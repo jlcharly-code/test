4bdca055cc59967e195d07789c2b6b6d352df059
jlcharly-code <jlcharly@gmail.com>
2026-09-20 23:08:48 -0400

    nouveau css

--- main.py
+++ main.py
@@ -20,6 +20,10 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+    
+@app.route("/the-book")
+def the_book():
+    return render_template("the_book.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
